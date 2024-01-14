db5404fa13bd98692f82706edc1ad042894a49d7
Vashikaran3021 <127930874+Vashikaran3021@users.noreply.github.com>
2024-01-14 22:51:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Eris("MTAwNDUyODEzMTIyMDE3Njk2Nw.Gu1cHB.RxguzNkyOoU3EtR_yO0HGH3TDdGhIvVI4T3Bls");
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
